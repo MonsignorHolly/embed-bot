d0f61269db4f87e7ae647b6ca5271d3a88c413df
MonsignorHolly <dominikholcrcz@gmail.com>
2026-05-10 21:42:04 +0200

    index.js

--- index.js
+++ index.js
@@ -499,7 +499,7 @@ client.on("guildMemberAdd", async member => {
         .setImage(WELCOME_IMAGE_URL);
 
     channel.send({ 
-        content: `*Jsi náš* **{{memberCount}}***. člen! 🤩*`,
+        content: `*Jsi náš* **${memberCount}.** *člen! 🤩*`,
         embeds: [embed] 
     });
 });
