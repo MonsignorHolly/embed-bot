fb87cbb1e7973a9ea14c6cc744c04a949abee14a
MonsignorHolly <dominikholcrcz@gmail.com>
2026-05-13 18:33:48 +0200

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require(“fs”);
-require(“dotenv”).config();
-const Groq = require(“groq-sdk”);
+const fs = require("fs");
+require("dotenv").config();
+const Groq = require("groq-sdk");
 
 const {
 Client,
